97d814030829876d0c0d29edd3d633c5e2eeb8bd
suns <suns@firsov.net>
2020-03-17 19:04:59 -0700

    post-build.mjs adjusts demo/ files

--- post-build.mjs
+++ post-build.mjs
@@ -1,23 +1,32 @@
 import fs from 'fs';
 import { readdirSync } from 'fs';
 
-const builds = JSON.parse(fs.readFileSync("polymer.json").toString()).builds;
+const builds = JSON.parse(fs.readFileSync("polymer.json").toString()).builds
+, buildNames = readdirSync('./build').filter( b=> b!=="polymer.json");
+
+buildNames.map( build=>
+    {   let stubName = `./build/${build}/index.html`
+        ,       stub = fs.readFileSync(stubName).toString()
+        ,   basePath = builds.find( b=> b.name == build ).basePath;
+
+        stub = stub.substring( stub.indexOf("<script>") )
+            .replace( /(\<\/head\>|\<body\>|\<\/body\>|\<\/html\>)/g ,"")
+            .replace( /src\=\"node_modules/g ,`src="${basePath}/node_modules`)
+            .replace( /src\=\"src/g ,`src="${basePath}/src`)
+            .replace( /define\(\w*\[\w*\'src/g ,`define(['${basePath}/src`)
+            .replace( /.indexOf\(\"\:\/\/\"\)/ ,`.indexOf("://")&&b.indexOf("/")`) // define( absUrl ) bug fix
+        ;
+        fs.writeFileSync( stubName, stub );
+    });
 
 readdirSync(`./demo`)
 .map( name =>
 {   const txt = fs.readFileSync(`./demo/${name}`).toString();
 
-    readdirSync('./build')
-    .filter( b=> b!=="polymer.json")
-    .map( build=>
+    buildNames.map( build=>
     {   let  fName = `./build/${build}/demo/${name}`
-        ,     stub = fs.readFileSync(`./build/${build}/index.html`).toString()
-        , basePath = builds.find( b=> b.name == build ).basePath;
-
-        stub = stub.substring( stub.indexOf("<script>") )
-            .replace( /(\<\/head\>|\<body\>|\<\/body\>|\<\/html\>)/g ,"")
-            .replace( /src\=\"node_modules/g ,`src="${basePath}/node_modules`)
-            .replace( /src\=\"src/g ,`src="${basePath}/src`);
+        , stubName = `./build/${build}/index.html`
+        ,     stub = fs.readFileSync(stubName).toString();
         fs.writeFileSync( fName, stub + txt );
     })
 });
