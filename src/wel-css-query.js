72833c1be6cc6a3e69cb1ce67f3c9461042b7c63
suns <suns@firsov.net>
2020-04-23 16:47:47 -0700

    wel-css-query.js polymer binding support added

--- src/wel-css-query.js
+++ src/wel-css-query.js
@@ -1,7 +1,9 @@
 export default class WelCssQuery extends HTMLElement
-{   //   <wel-css-query src="#cornerButton" dst="#menu1" dstprop="anchor" ></wel-css-query>
-
-    static get observedAttributes(){ return ['src','dst','srcprop','dstprop']}
+{   //   native binding:
+    //      <wel-css-query src="#cornerButton" dst="#menu1" dstprop="anchor" ></wel-css-query>
+    //   polymer binding:
+    //      <wel-css-query json='["from","strings","array"]' value="{{ demoValue::change }}"></wel-css-query>
+    static get observedAttributes(){ return ['src','dst','srcprop','dstprop'] }
     connectedCallback()
     {   let  a = x=>x && this.getAttribute(x)
         ,    $ = x=>document.querySelector(x)
@@ -9,7 +11,10 @@ export default class WelCssQuery extends HTMLElement
         ,   sp = a('srcprop')
         ,    d = a('dst'    )
         ,   dp = a('dstprop')
+        ,    j = a('json')
         ,    v = s && $(s);
+        if( !v && j )
+            v = JSON.parse( j );
         if( sp )
             v = v[ sp ];
         this.value = v;
@@ -19,5 +24,7 @@ export default class WelCssQuery extends HTMLElement
                 d[ dp ] = v;
             else
                 d = v;
-}   }   }
+        }
+        this.dispatchEvent( new CustomEvent('change') )
+}   }
 window.customElements.define( 'wel-css-query', WelCssQuery );
