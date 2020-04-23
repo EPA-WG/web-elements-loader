6884fcaf24eaff9a7283a20bbb9518d84f64d0a2
suns <suns@firsov.net>
2020-04-22 20:31:18 -0700

    wel-css-query.js added

--- src/wel-css-query.js
+++ src/wel-css-query.js
@@ -1,26 +1,23 @@
-class WelCssQuery extends HTMLElement
-{
+export default class WelCssQuery extends HTMLElement
+{   //   <wel-css-query src="#cornerButton" dst="#menu1" dstprop="anchor" ></wel-css-query>
+
     static get observedAttributes(){ return ['src','dst','srcprop','dstprop']}
     connectedCallback()
-    {   let  s = this.getAttribute('src'    )
-        ,   sp = this.getAttribute('srcprop')
-        ,    d = this.getAttribute('dst'    )
-        ,   dp = this.getAttribute('dstprop');
-
-        let v = s && document.querySelector(s);
+    {   let  a = x=>x && this.getAttribute(x)
+        ,    $ = x=>document.querySelector(x)
+        ,    s = a('src'    )
+        ,   sp = a('srcprop')
+        ,    d = a('dst'    )
+        ,   dp = a('dstprop')
+        ,    v = s && $(s);
         if( sp )
             v = v[ sp ];
         this.value = v;
+        d = $( d );
         if( d )
-        {   d = document.querySelector( d );
-            if( d )
-            {   if( dp )
-                    d[ dp ] = v;
-                else
-                    d = v;
-            }
-        }
-    }
-}
-window.customElements.define( 'wel-css-query', class extends HTMLElement
-export default WelCssQuery;
+        {   if( dp )
+                d[ dp ] = v;
+            else
+                d = v;
+}   }   }
+window.customElements.define( 'wel-css-query', WelCssQuery );
