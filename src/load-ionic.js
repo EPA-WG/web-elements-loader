c986bb8f2a5c2126fa5770c498130948a1ec29be
suns <suns@firsov.net>
2020-05-03 00:51:54 -0700

    dark mode switch

--- src/load-ionic.js
+++ src/load-ionic.js
@@ -11,18 +11,14 @@ class LoadIonic extends LoadCollection
 {
 
     static get is(){ return 'load-ionic'}
-    docs(pkg){ let n=pkg.name.split('/').pop(); return `https://github.com/vaadin/${ n }` }
-    isDisabledByDefault(pkg)
-    {
-        // return  [   '@vaadin/vaadin/vaadin'
-        //         ].includes(pkg)
-    }
+    docs(pkg){ return 'https://ionicframework.com/docs' }
+    isDisabledByDefault(pkg){}
     initDependencies()
     {   const init= i => this.initModule(i);
         return [
-        // init(x=>import("@stencil/core/"                                                      )),
-        init(x=>import("@ionic/core/dist/ionic/ionic.esm.js"                )),
-        // init(x=>import("@ionic/core/dist/esm/ionic.mjs"                )),
+        // init(x=>import("@stencil/core/"                         )),
+        init(x=>import("@ionic/core/dist/ionic/ionic.esm"       )),
+        // init(x=>import("@ionic/core/dist/esm/ionic.mjs"         )),
         ]
     }
 
